3d65bfe59355358c693f8c7a5ad9050e8c3d6e4a
Cookieseller <mathias@riedle-mainz.de>
2022-02-07 21:36:47 +0100

    Unified gate and process step

--- src/process/process.py
+++ src/process/process.py
@@ -2,6 +2,7 @@ from typing import Final
 
 from treelib import Tree
 
+from src.gate.gate import Gate
 from src.process.step.step import Step
 
 
@@ -17,19 +18,33 @@ class Process:
     def execute(self) -> None:
         try:
             for step in self.tree.children(self.ROOT_IDENTIFIER):
-                step.data.execute()
+                process_data = step.data
+                result = process_data.get_step().execute()
+                process_data.get_gate().gate(result)
         except AttributeError:
             #should never happen if a step was added
             pass
 
     def add_next_step(self, step: Step, gate) -> None:
-        self.tree.create_node(step.get_name(), step.get_identifier(), data=step, parent=self.last_added_identifier)
+        self.tree.create_node(step.get_name(), step.get_identifier(), data=self._ProcessStep(step, gate), parent=self.last_added_identifier)
         self.last_added_identifier = step.get_identifier()
 
     def add_root_step(self, step: Step, gate) -> None:
-        self.tree.create_node(step.get_name(), step.get_identifier(), data=step, parent=self.ROOT_IDENTIFIER)
+        self.tree.create_node(step.get_name(), step.get_identifier(), data=self._ProcessStep(step, gate), parent=self.ROOT_IDENTIFIER)
         self.last_added_identifier = step.get_identifier()
 
     def add_step_after(self, step: Step, previous_step: Step, gate) -> None:
-        self.tree.create_node(step.get_name(), step.get_identifier(), data=step, parent=previous_step.get_identifier())
+        self.tree.create_node(step.get_name(), step.get_identifier(), data=self._ProcessStep(step, gate), parent=previous_step.get_identifier())
         self.last_added_identifier = step.get_identifier()
+
+    class _ProcessStep:
+        def __init__(self, step: Step, gate: Gate) -> None:
+            super().__init__()
+            self.step = step
+            self.gate = gate
+
+        def get_step(self):
+            return self.step
+
+        def get_gate(self):
+            return self.gate
