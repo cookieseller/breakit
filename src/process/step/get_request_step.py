d7c644a043d7ef62e6eb696afe9b169a073cee91
Cookieseller <mathias@riedle-mainz.de>
2022-03-16 23:37:46 +0100

    Added header mutator to get request

--- src/process/step/get_request_step.py
+++ src/process/step/get_request_step.py
@@ -1,15 +1,16 @@
 from src.process.step.step import Step
+from src.request.header.header_mutator import HeaderMutator
 from src.request.request_factory import RequestFactory
 
 
 class GetRequestStep(Step):
 
-    def __init__(self, url: str) -> None:
+    def __init__(self, url: str, header_mutator: HeaderMutator) -> None:
         self.url = url
-        self.request = RequestFactory().create_get_request(url)
+        self.header_mutator = header_mutator
 
     def get_identifier(self) -> str:
         return f"{self.get_name()}{self.url}"
 
     def execute(self):
-        return self.request()
+        return RequestFactory().create_get_request(self.url, self.header_mutator.mutate(''))
