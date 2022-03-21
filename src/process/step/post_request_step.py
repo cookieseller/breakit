41b75dff7bc48c14e10d20c8fe17e049562efdc4
Cookieseller <mathias@riedle-mainz.de>
2022-03-21 20:59:58 +0100

    Added header and body mutator to post request

--- src/process/step/post_request_step.py
+++ src/process/step/post_request_step.py
@@ -1,12 +1,16 @@
 from src.process.step.step import Step
+from src.request.body.mutator.body_mutator import BodyMutator
+from src.request.header.header_mutator import HeaderMutator
 from src.request.request_factory import RequestFactory
 
 
 class GetRequestStep(Step):
 
-    def __init__(self, url: str) -> None:
+    def __init__(self, url: str, header_mutator: HeaderMutator = None, body_mutator: BodyMutator = None) -> None:
         self.url = url
         self.request = RequestFactory().create_post_request(url)
+        self.header_mutator = header_mutator
+        self.body_mutator = body_mutator
 
     def get_identifier(self) -> str:
         return f"{self.get_name()}{self.url}"
