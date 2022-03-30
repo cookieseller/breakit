2df67a782a73251ee923a96f8bc6fdcbfbd7e805
Cookieseller <mathias@riedle-mainz.de>
2022-03-30 23:07:23 +0200

    Loading from persisted env

--- src/process/storage_container/persisted_environment.py
+++ src/process/storage_container/persisted_environment.py
@@ -3,15 +3,23 @@ import json
 from src.process.storage_container.environment import Environment
 
 
-class PersistedEnvironment:
+class PersistedEnvironment(Environment):
     def __init__(self, file_name):
-        self.environment = Environment()
+        super().__init__()
         self.file_name = file_name
 
     def __enter__(self):
+        with open(self.file_name, 'w') as f:
+            data = f.read()
+
+        data_dict = json.loads(data)
+        for key, value in data_dict:
+            self.set_value(key, value)
+
         self.file = open(self.file_name, 'w')
-        return self.file
+
+        return self
 
     def __exit__(self):
-        self.file.write(json.dumps(self.environment.get_all()))
+        self.file.write(json.dumps(self.get_all()))
         self.file.close()
