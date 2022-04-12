ca99b32592e7be8b2990f680e14acb6dee4ba8a9
Cookieseller <mathias@riedle-mainz.de>
2022-04-12 22:56:32 +0200

    Renamed mutator

--- src/flows/signup_flow.py
+++ src/flows/signup_flow.py
@@ -6,7 +6,7 @@ from src.process.process import Process
 from src.process.step.get_request_step import GetRequestStep
 from src.process.response_parsers.json_variable_extractor import VariableExtractor
 from src.process.variable_storage.persisted_environment import PersistedEnvironment
-from src.request.header.common_header_mutator import CommonHeaderMutator
+from src.request.header.common_user_agent_mutator import CommonUserAgentMutator
 from src.request.header.multi_header_mutator import MultiHeaderMutator
 
 process = Process()
@@ -14,16 +14,16 @@ process = Process()
 with PersistedEnvironment('env') as env:
     process.add_default_response_parser(ResponseCodeExtractor(env))
     # Add request body mutator
-    process.add_step(GetRequestStep('http://localhost:8080/', CommonHeaderMutator()))\
+    process.add_step(GetRequestStep('http://localhost:8080/', CommonUserAgentMutator()))\
         .set_step_validator(OkResponseGate(env))
-    process.add_step(GetRequestStep('http://localhost:8080/start', CommonHeaderMutator()))\
+    process.add_step(GetRequestStep('http://localhost:8080/start', CommonUserAgentMutator()))\
         .set_step_validator(OkResponseGate(env))
-    process.add_step(GetRequestStep('http://localhost:8080/gettoken', CommonHeaderMutator()))\
+    process.add_step(GetRequestStep('http://localhost:8080/gettoken', CommonUserAgentMutator()))\
         .add_response_parser(VariableExtractor(env))\
         .set_step_validator(OkResponseGate(env))
-    process.add_step(GetRequestStep('http://localhost:8080/validatetoken', MultiHeaderMutator([CommonHeaderMutator()])))\
+    process.add_step(GetRequestStep('http://localhost:8080/validatetoken', MultiHeaderMutator([CommonUserAgentMutator()])))\
         .set_step_validator(BadRequestResponseGate(env))
-    process.add_step(GetRequestStep('http://localhost:8080/end', CommonHeaderMutator()))\
+    process.add_step(GetRequestStep('http://localhost:8080/end', CommonUserAgentMutator()))\
         .set_step_validator(BadRequestResponseGate(env))
 
     process.execute()
--- src/request/header/common_user_agent_mutator.py
+++ src/request/header/common_user_agent_mutator.py
@@ -3,7 +3,7 @@ import random
 from src.request.header.header_mutator import HeaderMutator
 
 
-class CommonHeaderMutator(HeaderMutator):
+class CommonUserAgentMutator(HeaderMutator):
     def mutate(self, _=None):
         headers = [
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
